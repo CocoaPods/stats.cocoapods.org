9e208e28dca7f34d58aa9a524e7060fbbeae0e24
Orta Therox <orta.therox@gmail.com>
2015-05-09 17:02:49 +0100

    Actually call analytics

--- app/analytics.rb
+++ app/analytics.rb
@@ -4,11 +4,11 @@ require 'segment/analytics'
 module Pod
   class Analytics
     def self.track(options)
-      # Segment::Analytics.track options
+      Segment::Analytics.track options
     end
     
     def self.identify(options)
-      # Segment::Analytics.identify options
+      Segment::Analytics.identify options
     end
   end    
 end
